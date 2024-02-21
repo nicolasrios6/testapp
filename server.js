518d0140588f47bfc440a99726d834315c8aa537
Nicolás Ríos <riosnicolas618@gmail.com>
2024-02-21 12:05:59 -0300

    agrego firebase realtime database

--- server.js
+++ server.js
@@ -2,16 +2,25 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 5500 });
 
-wss.on('connection', function connection(ws) {
-  console.log('Cliente conectado');
+let scores = [];
 
-  ws.on('message', function incoming(message) {
-    console.log('Mensaje recibido: %s', message);
-    // Envía el mensaje a todos los clientes, incluido el que lo envió
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+wss.on('connection', function connection(ws) {
+    console.log('Cliente conectado');
+
+    ws.on('message', function incoming(message) {
+        console.log('Mensaje recibido: %s', message);
+        const data = JSON.parse(message);
+        if (data.type === "score") {
+            scores.push({ playerName: data.playerName, score: data.score });
+            sendScoreboard();
+        }
     });
-  });
 });
+
+function sendScoreboard() {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "scoreboard", scoreboard: scores }));
+        }
+    });
+}
